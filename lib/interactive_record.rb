17ef099a6224c431c59b0d614b66bccd578be370
Trent Moss <trentmoss05@gmail.com>
2019-02-18 16:01:41 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -6,4 +6,25 @@ class InteractiveRecord
   def self.table_name
   end
 
+  def self.column_names
+  end
+
+  def table_name_for_insert
+  end
+
+  def col_names_for_insert
+  end
+
+  def values_for_insert
+  end
+
+  def save
+  end
+
+  def self.find_by_name
+  end
+
+  def self.find_by
+  end
+
 end
