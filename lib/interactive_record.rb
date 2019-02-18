2fdf980613dec246c1d591c233d89e8d141cfad9
Trent Moss <trentmoss05@gmail.com>
2019-02-18 16:24:16 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -4,34 +4,35 @@ require 'active_support/inflector'
 class InteractiveRecord
 
   def self.table_name
-    self.to_s.downcase.pluralize
+    "#{self.to_s.downcase}s"
   end
 
   def self.column_names
-    DB[:conn].results_as_hash = true
-    sql = "PRAGMA table_info('#{table_name}')"
+
+    sql = "pragma table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
     column_names = []
-
-    table_info.each do |column|
-      column_names << column["name"]
+    table_info.each do |row|
+      column_names << row["name"]
     end
     column_names.compact
   end
 
   def initialize(options={})
-    options.each do |p, v|
-      self.send("#{p}=", v)
+    options.each do |property, value|
+      self.send("#{property}=", value)
     end
+  end
+
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
   def table_name_for_insert
     self.class.table_name
-  end
-
-  def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
   def values_for_insert
@@ -42,10 +43,8 @@ class InteractiveRecord
     values.join(", ")
   end
 
-  def save
-    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
-    DB[:conn].execute(sql)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
   def self.find_by_name(name)
