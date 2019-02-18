d5d7f0b08d7222aae764b94329c6f287ddf211d2
Trent Moss <trentmoss05@gmail.com>
2019-02-18 16:12:10 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -35,6 +35,11 @@ class InteractiveRecord
   end
 
   def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
   end
 
   def save
