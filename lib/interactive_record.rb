f68e068c074928f40f07ff4e8d2ce3b591542320
Trent Moss <trentmoss05@gmail.com>
2019-02-18 16:06:13 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -8,6 +8,16 @@ class InteractiveRecord
   end
 
   def self.column_names
+    DB[:conn].results_as_hash = true
+    sql = "PRAGMA table_info('#{table_name}')"
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+
+    table_info.each do |column|
+      column_names << column["name"]
+    end
+    column_names.compact
   end
 
   def table_name_for_insert
